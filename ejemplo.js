602ff2a1ce686072d9571f0e363cc118fd527c5e
FlorenciaRossi <flrossi@estudiantes.unsam.edu.ar>
2025-09-25 19:34:26 -0300

    js example

---
+++ ejemplo.js
@@ -0,0 +1,36 @@
+class Empleado {
+    constructor(nombre, salario) {
+        this.nombre = nombre;
+        this.salario = salario;
+    }   
+    calcularBono() {
+        return this.salario * 0.1;
+    }
+
+    info() {
+        return this.nombre + " -> Bono: $" + this.calcularBono();
+    }
+}
+
+class Gerente extends Empleado {
+    calcularBono() {
+        return super.calcularBono() + (this.salario * 0.2);
+    }
+}
+
+
+class Programador extends Empleado {
+    extraBono = 50000;
+
+    calcularBono() {
+        return super.calcularBono() + this.extraBono;
+    }
+}
+
+let gerente = new Gerente("Carlos Pérez", 800000)
+let programador = new Programador("Lucía Gómez", 600000)
+const empleados = [gerente, programador];
+
+for (const empleado of empleados) {
+    console.log(empleado.info());
+}
---
+++ ejemplo.md
@@ -0,0 +1,20 @@
+# 📌 Ejercicio: Bonos anuales de empleados
+
+Queremos calcular los **bonos anuales** de los empleados de la empresa ficticia **NosceTeIpsum**.
+
+### Reglas del negocio
+- A todos los empleados, sin importar su puesto, se les da un bono anual que representa el **10% de su sueldo**.  
+- Para los **Gerentes**, el bono es del **20% de su sueldo**.  
+- Para los **Programadores**, el bono es un monto fijo de **$50.000**.  
+
+---
+
+### Se pide
+1. **Modelar las clases** de los empleados.  
+2. **Instanciar** los siguientes empleados:  
+
+   ```js
+   let gerente = new Gerente("Carlos Pérez", 800000);
+   let programador = new Programador("Lucía Gómez", 600000);
+
+Nota: Para correr el código en la terminal ejecutamos el comando 'node tuArchivo.js'
